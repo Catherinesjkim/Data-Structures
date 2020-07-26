5275961d02a7646bcd993dcfd58cf622e2855988
Catherine Kim <catherine@ipwe.com>
2020-07-26 14:09:58 -0700

    Add Updated SLL Because: - Added some comments and made sure the tests are passing.

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -8,127 +8,132 @@
 # All LL/class Node has to know is somewhere needs to store value + where the next node is. 
 
 # 2 Classses - Node & LL
-
 # Node = value & next 
+# self == whole Node (value and next_node)
 class Node: 
     def __init__(self, value=None, next_node=None): # pass in value and nex_node, default to None
+        # the value at this linked list node
         self.value = value
+        # reference to the next node in the list
         self.next_node = next_node
     
     # getter methods
-    def get_value(self):
-        return self.value
+    # def get_value(self):
+    #     return self.value
     
-    def get_next(self):
-        return self.next_node
+    # def get_next(self):
+    #     return self.next_node
     
-    def set_next(self, new_next)
-        self.next_node = new_next
-
-# my_node = Node(1, Node())
+    # def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        # self.next_node = new_next
 
 # LL obj needs to keep track of the Head and Tail
 # LinkedList class is like a node manager - add, delete, etc.
 # What is head going to store? A node that corresponse to our first node in the list
 
-# What do we need? Head & Tail
-# only method needed: add_to_tail or remove_head
+# What do we need? Head & Tail, variable pointing to the node
+# The only method needed: add_to_tail or remove_head
+# Dependent on the Node class that we have above
 class LinkedList:
-    def __init__(self):
+    def __init__(self): # initialize an empty LL
+        # reference to the head of the list
         self.head = None # stores a node, that corresponds to our first node in the list
+        # reference to the tail of the list
         self.tail = None # stores a node that is the end of the list
     
+    # logic that interacts with variables head & tail
     # like an append/prepend/shift method in an array
     # Insertion - keep track of head
     def add_to_head(self, value):
         # create a node to add to head
         new_node = Node(value)
         # check if list is empty
-        if  self.head is None and self.tail is None:
+        if self.head is None and self.tail is None:
             self.head = new_node
             self.tail = new_node
         else: 
             # new_node should point to current head
             new_node.next_node = self.head
-            # move ehad to new node
+            # move ahead to new node
             self.head = new_node
     
     # Insertion - keep track of tail
     def add_to_tail(self, value):
-        # create a node to add to tail
-        new_node = Node(value, None)  # LL that has 1 item so far value == None
-        # check if there is no head - empty list
-        if not self.head: # LL that has 1 item, the head & tail are the same
+        # wrap the input value in a node
+        new_node = Node(value) 
+        # check if there is no head (i.e., the list is empty)
+        if self.head is None and self.tail is None: 
+            # if the list is initially empty, set both head and tail to the nnew_node
             self.head = new_node
-            self.tail = new_node
-            
-        # otherwise (if not an empty list), we will always have a tail
+            self.tail = new_node 
+        # we have a non-empty list, add the new node to the tail
         else: 
-            # point the node at the current tail, to the new node
-            self.tail.set_next(new_node)
+            # set the current tail's next reference to our new node
+            self.tail.next_node = new_node
+            # set the list's tail reference to the new node
             self.tail = new_node
     
     # Delete - remove the head and return its value
     # why do we want to remove the head? Append: change the head pointer
-    def remove_head(self):
-        # is ther a head? 
+    def remove_head(self): # self == The LL instance
+        # No Item: return None if there's no head (i.e. the list is empty == there's no node)  
         if not self.head:
-            return None # empty
-        
-        # if head has no next, there is a single element in the LL
-        if not self.head.get_next():
-            head = self.head # 1 single item
-            self.head None
+            return None 
+        # One Item: if head has no next, then we have a single element in our list
+        if self.head.next_node is None:
+            # get a reference to the head
+            head_value = self.head.value # 1 single item
+            # delete the list's head reference
+            self.head = None
+            # also make sure the tail reference doesn't refer to anything
             self.tail = None 
-            
-            return head.get_value() # return zero element list - single LL
-        
-        # more than one item 
-        value = self.head.get_value()
+            # return the value
+            return head_value
+        # Else: More than one item: otherwise we have more than one element in our list 
+        head_value = self.head.value # value === just the string, not the node
         # set the head reference to the current head's next node in the list
-        self.head = self.head.get_next()
-        return value
-            
+        self.head = self.head.next_node
+        return head_value
+        
     # traverse that some element exists, we did it correctly by adding a value and see that it exists
     # need to Search through the LL - no divide & conquer, need to do it one by one
     # contains = true or false
     def contains(self, value):
-        if not self.head:
+        if self.head is None:
             return False
-        
         # get a reference to the node we're currently at; update this as we traverse the
-        current = self.head
+        current_node = self.head
         # check to see if we're at a valid node 
-        while current:
+        while current_node is not None:
         # return True if the current value we're looking at matches our target value
-            if current.get_value == value:
+            if current_node.value == value:
                 return True
-            
             # update our current node to the current node's next node
-            current = current.get_next()
+            current_node = current_node.next_node
         # if we've gotten here, then the target node isn't in our list
         return False
     
     def get_max(self):
         current = self.head
-        
+    
         if (self.head == None):
             print("List is empty")
         else:
             # initializing max to initial node data
             max = self.head.value
             while(True):
-                # if current node's value is greater than max, then replace value of max with current nodes' value
+                # if current node's value is greater than max, then replace value of max with current nodes values
                 if(max < current.value):
-                   max = current.value
+                    max = current.value
                 if (current == self.tail):
-                    break # because the tail in singly_linked_list doesn't have a pointer/reference to the next node
+                    break
                 current = current.next_node
             print("Maximum value node in the list: " + str(max))
             
             return max
+        
 
-# print out the results of the methods
 linked_list = LinkedList()
 
 linked_list.add_to_head(0)
@@ -143,8 +148,7 @@ print(f'The start of the list is {linked_list.head.value}')
 linked_list.add_to_head(5)
 print(f'The start of the list is {linked_list.head.value}')
 
-# linked_list.remove_head()
-# print(f'The start of the list is {linked_list.head.value}')
+linked_list.remove_head()
+print(f'The start of the list is {linked_list.head.value}')
 
 linked_list.get_max()
-# print("Maximum value node in the list: " + str(max))

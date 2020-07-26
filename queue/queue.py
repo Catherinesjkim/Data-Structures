91a5c6b5440ba6a0f9d529aac35cc993595bd658
Catherine Kim <catherine@ipwe.com>
2020-07-26 14:53:26 -0700

    Add Updated Queue file Because: - Implemented the Queue class using built-in Python lists and the Node and LL classes I created during the Module 1 Guided Project. This commit: - ArrayQueue class has the push, pop and len methods - Corrections: ArrayQueue class has a enqueue, dequeue and len methods.

--- queue/queue.py
+++ queue/queue.py
@@ -1,4 +1,17 @@
 """
+- Linear
+- FIFO (First In, First Out)
+- Checkout line at the grocery stores
+
+Example operations:
+
+    enqueue(item) - adding an item to the end of the queue
+    dequeue() - removing the item at the beginning of the queue
+    
+All queue operations take O(1) time. 
+"""
+
+"""
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order (FIFO) i.e. printer, back button, and undo hotkey.  
 
@@ -17,7 +30,7 @@ import sys
 sys.path.append('../singly_linked_list')
 from singly_linked_list import LinkedList
 
-class ArrayQueue:
+class Queue:
     def __init__(self):
         self.size = 0
         self.storage = []
@@ -34,19 +47,26 @@ class ArrayQueue:
             return None
         return self.storage.pop(0)
         
+        
+# Queue implementaiton using a LL
+# LL starts with 2 Nones
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
 
-class LLQueue:
-    def __init__(self):
-        self.size = 0
-        self.storage = LinkedList()
+#     def __len__(self):
+#         return self.size
 
-    def __len__(self):
-        return self.size
+#     # if someone get into a line
+#     def enqueue(self, item):
+#         # add the item to the LL
+#         self.storage.add_to_tail(item)
+#         self.size += 1
 
-    def enqueue(self, value):
-        self.storage.add_to_tail(value)
-
-    # how do we remove from a queue?
-    def dequeue(self):
-        if len(self.storage) == 0:
-            return None
+#     # how do we remove from a queue?
+#     def dequeue(self): # It's my turn to checkout
+#         if self.size == 0:
+#             return None
+#         self.size -= 1
+#         return self.storage.remove_head() # LL taking care of the pointers
